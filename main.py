9e7db4f8124de72e4c05bba9d3e677ffb3e9e1a6
Brian Jason Valerio Montes <brianjasonmontes@gmail.com>
2025-04-09 17:59:16 -0600

    Version 2: Ver tareas

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 print("Que pasa chavales todo bien todo correcto y yo que me alegro. Bienvenidos al mejor Gestor de Tareas.")
 
-tarea = input("Escribe una nueva tarea: ")
-with open("tareas.txt", "a") as archivo:
-    archivo.write(tarea + "\n")
-    print("Tarea guardada.")
+print("\nTus tareas:")
+with open("tareas.txt", "r") as archivo:
+    print(archivo.read())
