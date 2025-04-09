3105204eee24923ef93e29aa89252a6e772c139f
Brian Jason Valerio Montes <brianjasonmontes@gmail.com>
2025-04-09 17:55:06 -0600

    Version 1: Guardar tareas

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 print("Que pasa chavales todo bien todo correcto y yo que me alegro. Bienvenidos al mejor Gestor de Tareas.")
+
+tarea = input("Escribe una nueva tarea: ")
+with open("tareas.txt", "a") as archivo:
+    archivo.write(tarea + "\n")
+    print("Tarea guardada.")
